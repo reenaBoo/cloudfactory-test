12a43535af5c6daddff3d646b0e4a9cd335870e3
Karina Lyapina <46646742+reenaBoo@users.noreply.github.com>
2021-10-25 10:25:19 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 import { Root, StyledLink } from './Header.styles';
 
-
 function Header() {
-    return (
-        <Root>
-            <StyledLink to='/about'>О приложении</StyledLink>
-            <StyledLink to='/quotes'>Котировки</StyledLink>
-        </Root>
-    )
+  return (
+    <Root>
+      <StyledLink to='/about'>О приложении</StyledLink>
+      <StyledLink to='/quotes'>Котировки</StyledLink>
+    </Root>
+  )
 }
 
 export default Header;
