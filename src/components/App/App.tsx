75dd1a0d14e74d188fe30d32fd3d91e01dee6eba
Karina Lyapina <ivettra@yandex.ru>
2022-03-23 22:42:16 +0500

    feat: convert to ts/tsx

---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import About from '../About/About';
+import Quotes from '../Quotes/Quotes';
+import Header from '../Header/Header';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/quotes" element={<Quotes />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
